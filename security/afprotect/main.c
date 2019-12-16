bfe07f305f3eb1253f6f840e1c5f77f5c987cdf2
Anton Suslov <anton_suslov@me.com>
2019-12-16 23:20:59 +0300

    Added basic security module

---
+++ security/afprotect/main.c
@@ -0,0 +1,92 @@
+//INCLUDES/////////////////////////////////////////////////////////////////////
+
+#include <linux/init.h>
+#include <linux/kd.h>
+#include <linux/kernel.h>
+#include <linux/tracehook.h>
+#include <linux/errno.h>
+#include <linux/ext2_fs.h>
+#include <linux/sched.h>
+#include <linux/security.h>
+#include <linux/xattr.h>
+#include <linux/capability.h>
+#include <linux/unistd.h>
+#include <linux/mm.h>
+#include <linux/mman.h>
+#include <linux/slab.h>
+#include <linux/pagemap.h>
+#include <linux/proc_fs.h>
+#include <linux/swap.h>
+#include <linux/spinlock.h>
+#include <linux/syscalls.h>
+#include <linux/dcache.h>
+#include <linux/file.h>
+#include <linux/fdtable.h>
+#include <linux/namei.h>
+#include <linux/mount.h>
+#include <linux/netfilter_ipv4.h>
+#include <linux/netfilter_ipv6.h>
+#include <linux/tty.h>
+#include <net/icmp.h>
+#include <net/ip.h>             /* for local_port_range[] */
+#include <net/tcp.h>            /* struct or_callable used in sock_rcv_skb */
+#include <net/net_namespace.h>
+#include <net/netlabel.h>
+#include <linux/uaccess.h>
+#include <asm/ioctls.h>
+#include <asm/atomic.h>
+#include <linux/bitops.h>
+#include <linux/interrupt.h>
+#include <linux/netdevice.h>    /* for network interface checks */
+#include <linux/netlink.h>
+#include <linux/tcp.h>
+#include <linux/udp.h>
+#include <linux/dccp.h>
+#include <linux/quota.h>
+#include <linux/un.h>           /* for Unix socket types */
+#include <net/af_unix.h>        /* for Unix socket types */
+#include <linux/parser.h>
+#include <linux/nfs_mount.h>
+#include <net/ipv6.h>
+#include <linux/hugetlb.h>
+#include <linux/personality.h>
+#include <linux/audit.h>
+#include <linux/string.h>
+#include <linux/selinux.h>
+#include <linux/mutex.h>
+#include <linux/posix-timers.h>
+#include <linux/syslog.h>
+#include <linux/user_namespace.h>
+#include <linux/lsm_hooks.h>
+
+
+//DEFINES//////////////////////////////////////////////////////////////////////
+
+#define MODULE_NAME "afprotect"
+
+
+//ABOUT////////////////////////////////////////////////////////////////////////
+
+MODULE_AUTHOR("afakeman");
+MODULE_DESCRIPTION("Simple Linux Security Module");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("0.1.2");
+
+
+//HOOKS/////////////////////////////////////////////////////////////////////////
+
+#include "definitions.include"
+#include "hooks.include"
+
+//INIT/////////////////////////////////////////////////////////////////////////
+
+static __init int ptlsm_init(void)
+{
+        security_add_hooks(stub_hooks, ARRAY_SIZE(stub_hooks), "afprotect");
+
+		printk(KERN_ALERT "AFProtect started");
+
+		return 0;
+}
+
+security_initcall(ptlsm_init);
